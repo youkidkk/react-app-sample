a79845593cf8de1c9ac54a348eaf9b5fb3696cb5
youkidkk <youkidkk80@gmail.com>
2021-07-24 08:19:05 +0900

    Material-UI でレイアウトを書き換え

--- src/App.js
+++ src/App.js
@@ -2,35 +2,81 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 
 import logo from "./logo.svg";
 import "./App.css";
+import "@fontsource/roboto/500.css";
 
 import ReduxSample from "./redux/redux-sample";
 
+import {
+  AppBar,
+  IconButton,
+  List,
+  ListItem,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  nav: {
+    width: 300,
+  },
+  main: {
+    flex: 1,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
     <BrowserRouter>
-      <div className="Nav">
-        <Link to="/">Home</Link>
-        <Link to="/redux">Redux</Link>
-      </div>
-      <Route exact path="/">
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+      <CssBaseline />
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">React App Sample</Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <div className={classes.root}>
+        <nav className={classes.nav}>
+          <List>
+            <ListItem>
+              <Link to="/">Home</Link>
+            </ListItem>
+            <ListItem>
+              <Link to="/redux">Redux</Link>
+            </ListItem>
+          </List>
+        </nav>
+        <div className={classes.main}>
+          <Route exact path="/">
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </header>
+            </div>
+          </Route>
+          <Route path="/redux" component={ReduxSample} />
         </div>
-      </Route>
-      <Route path="/redux" component={ReduxSample} />
+      </div>
     </BrowserRouter>
   );
 }
