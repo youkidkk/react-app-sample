0f1e9dbaa5cfd86b1cdde622f6d7cdc9d05d8452
youkidkk <youkidkk80@gmail.com>
2021-07-25 17:10:31 +0900

    セレクトボックスの値と初期値を変更

--- src/form/form.js
+++ src/form/form.js
@@ -28,11 +28,10 @@ export default function Form(props) {
           <TextField id="age" label="年齢" type="number" />
         </div>
         <div>
-          <TextField id="gender" select label="性別">
-            <MenuItem />
-            <MenuItem value={1}>男性</MenuItem>
-            <MenuItem value={2}>女性</MenuItem>
-            <MenuItem value={3}>他</MenuItem>
+          <TextField id="gender" select label="性別" defaultValue="3">
+            <MenuItem value="1">男性</MenuItem>
+            <MenuItem value="2">女性</MenuItem>
+            <MenuItem value="3">他</MenuItem>
           </TextField>
         </div>
       </form>
