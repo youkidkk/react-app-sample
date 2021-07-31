c0a5d7cd1c84d44901226cf056cf5f3ecc6673cd
youkidkk <youkidkk80@gmail.com>
2021-07-31 14:03:26 +0900

    フォームでの入力処理を仮実装

--- src/form/form.js
+++ src/form/form.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { MenuItem, TextField } from "@material-ui/core";
+import { Button, MenuItem, TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import ContentsTitle from "../common/contents-title";
@@ -16,21 +16,68 @@ const useStyles = makeStyles((theme) => ({
 export default function Form(props) {
   const classes = useStyles();
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [gender, setGender] = useState("");
+  const [birthday, setBirthday] = useState("1980-01-01");
+
+  function handleChange(event) {
+    switch (event.target.id) {
+      case "firstName":
+        setFirstName(event.target.value);
+        break;
+      case "lastName":
+        setLastName(event.target.value);
+        break;
+      case "birthday":
+        setBirthday(event.target.value);
+        break;
+      default:
+        break;
+    }
+  }
+
+  function handleGenderSelect(event) {
+    setGender(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(firstName);
+    console.log(lastName);
+    console.log(gender);
+    console.log(birthday);
+  }
+
   return (
     <React.Fragment>
       <ContentsTitle title="Form sample" />
-      <form className={classes.inputForm}>
+      <form className={classes.inputForm} onSubmit={handleSubmit}>
         <div>
-          <TextField id="lastName" label="姓" />
-          <TextField id="firstName" label="名" />
+          <TextField
+            id="lastName"
+            label="姓"
+            inputProps={{ required: true }}
+            value={lastName}
+            onChange={handleChange}
+          />
+          <TextField
+            id="firstName"
+            label="名"
+            inputProps={{ required: true }}
+            value={firstName}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <TextField
             id="gender"
             select
             label="性別"
-            defaultValue=""
             style={{ width: "4.0rem" }}
+            inputProps={{ required: true }}
+            value={gender}
+            onChange={handleGenderSelect}
           >
             <MenuItem value="1">男性</MenuItem>
             <MenuItem value="2">女性</MenuItem>
@@ -40,8 +87,15 @@ export default function Form(props) {
             id="birthDay"
             label="生年月日"
             type="date"
-            defaultValue="1980-01-01"
+            inputProps={{ required: true }}
+            value={birthday}
+            onChange={handleChange}
           />
+        </div>
+        <div>
+          <Button type="submit" variant="contained" color="primary">
+            確認
+          </Button>
         </div>
       </form>
     </React.Fragment>
