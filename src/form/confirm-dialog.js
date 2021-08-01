1bf22b1f3a6bb0fee4193855f7669ba646768dec
youkidkk <youkidkk80@gmail.com>
2021-08-01 11:32:04 +0900

    テーブルで表示するよう変更

--- src/form/confirm-dialog.js
+++ src/form/confirm-dialog.js
@@ -2,23 +2,47 @@ import {
   Dialog,
   DialogContent,
   DialogTitle,
-  List,
-  ListItem,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
 } from "@material-ui/core";
 
 export default function ConfirmDialog(props) {
   const { firstName, lastName, gender, birthday } = props.formInput;
+
   return (
     <Dialog open={props.open} onClose={props.onClose}>
       <DialogTitle>確認</DialogTitle>
       <DialogContent>
-        <List>
-          <ListItem>
-            姓名： {lastName} {firstName}
-          </ListItem>
-          <ListItem>性別： {gender}</ListItem>
-          <ListItem>生年月日： {birthday}</ListItem>
-        </List>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>項目</TableCell>
+                <TableCell>値</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>姓名</TableCell>
+                <TableCell>
+                  {lastName} {firstName}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>性別</TableCell>
+                <TableCell>{gender}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>生年月日</TableCell>
+                <TableCell>{birthday}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </DialogContent>
     </Dialog>
   );
